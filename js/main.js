d92a577c19f5c4a305ff62c96ff09a83ab1f504d
Nikita Gorelov <nikita.gl.pda@gmail.com>
2023-09-25 16:55:15 +0300

    remved bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.querySelector("#burger").addEventListener("click" , () => {
     document.querySelector(".header__mobile-list-menu").classList.toggle("active")
-    document.querySelector("body").classList.add("active")
+    document.querySelector("body").classList.toggle("active")
     document.querySelector(".burger").classList.toggle("active")
 } )
 document.querySelectorAll(".header__mobile-list-menu .menu-link").forEach((link) => {
