e6416f8f459a1a1913ed08eb5ade83e8fa31d810
Nikita Gorelov <nikita.gl.pda@gmail.com>
2023-09-25 16:08:23 +0300

    new repo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.querySelector("#burger").addEventListener("click" , () => {
+    document.querySelector(".header__mobile-list-menu").classList.toggle("active")
+    document.querySelector("body").classList.add("active")
+    document.querySelector(".burger").classList.toggle("active")
+} )
+document.querySelectorAll(".header__mobile-list-menu .menu-link").forEach((link) => {
+    link.addEventListener("click" , () => {
+        document.querySelector(".header__mobile-list-menu").classList.remove("active")
+        document.querySelector("body").classList.remove("active")
+        document.querySelector(".burger").classList.remove("active")
+    } )
+} ) 
