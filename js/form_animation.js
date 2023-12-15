8c2819bf531456bcbc6334639f3b21da6cb17ee2
Nikita Gorelov <nikita.gl.pda@gmail.com>
2023-12-15 16:44:12 +0300

    animation form done

--- js/form_animation.js
+++ js/form_animation.js
@@ -7,5 +7,13 @@ inputs.forEach((formInput)=>{
     })
 })  
     
+inputs.forEach((formInput)=>{
+    formInput.addEventListener("blur" , () => {
+        if (!formInput.value.trim().length > 0){
+        formInput.closest(".form__item")
+        .querySelector(".form__fake-placeholder")
+        .classList.remove("active")
+        }
+    })
+}) 
 
-console.log(inputs);
